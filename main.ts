a5665290de83a33b5a3eefd5881768023b94893f
CalliTGS3 <58722460+CalliTGS3@users.noreply.github.com>
2019-12-28 22:09:15 +0100

    Updated functions, timings, cycles and init values

--- main.ts
+++ main.ts
@@ -1,38 +1,41 @@
 /**
  * Benutzerdefinierte Blöcke
  */
-//% weight=100 color=#0fbc11 icon=""
+//% weight=100 color= #239b56 icon="\uf863"
 namespace sharpGP2Y1010AU0F {
-    const REFERENCE_VOLTAGE = 3; // V
-    const NODUST_VOLTAGE = 0.4;
+    const REFERENCE_VOLTAGE = 3000.0; // mV
+    const NODUST_VOLTAGE = 400.0; // mV
+    const CONVERSION_RATIO = 0.17; // ug/m3 / mV
     const WAVESHARE_DIVIDER = 11;
-    const SAMPLES = 10;
-    const PULSE_TIME = 320; // micro sec, SPEC
-    const SAMPLING_TIME = 280; // micro sec, SPEC
-    const DELTA_TIME = 40; // micro sec, PULSE_TIME - SAMPLING_TIME, valid for analogReadPin with ZERO runtime
-    const CYCLE_TIME = 10000; // micro sec, SPEC 
-    const SLEEP_TIME = 9680; // micro sec, SPEC: pulse cycle 10 ms (CYCLE_TIME - PULSE_TIME)     
+    const PULSE_TIME = 320; // us, SPEC
+    const SAMPLING_TIME = 280; // us, SPEC
+    const DELTA_TIME = 40; // us, PULSE_TIME - SAMPLING_TIME, valid for analogReadPin with ZERO runtime
+    const CYCLE_TIME = 10000; // ms, SPEC 
+    const SLEEP_TIME = 9680; // ms, SPEC: pulse cycle 10 ms (CYCLE_TIME - PULSE_TIME)     
     const VLED_ON = 1;
     const VLED_OFF = 0;
+    let SAMPLES = 10;
     let VLED = 0; // digital out PIN
     let VO = 0; // analog in PIN
 
     /**
          * get dust value (μg/m³) 
-         * @param vLED describe parameter here, eg: DigitalPin.P16 (microbit)
-         * @param vo describe parameter here, eg: AnalogPin.P1 (microbit)
+         * @param vLED describe parameter here, eg: DigitalPin.P16 (microbit), DigitalPin.P3 (calliope)
+         * @param vo describe parameter here, eg: AnalogPin.P1 (microbit), AnalogPin.P2 (calliope)
     */
     //% block
-    export function initDustSensor(vled: DigitalPin, vo: AnalogPin) {
+    export function initDustSensor(vled: DigitalPin, vo: AnalogPin, samples?: number) {
         VLED = vled;
         VO = vo;
+        if (samples) {
+            SAMPLES = samples;
+        }
     }
 
     //% block
-    export function getDustValue(): number {
-        let voltage = 0;
-        let sum_voltage = 0;
-        let dust = 0;
+    export function getSensorRAWValue(): number {
+        let voltage = 0.0;
+        let sum_voltage = 0.0;
         if ((VLED == 0) || (VO == 0)) {
             return 0
         }
@@ -50,11 +53,17 @@ namespace sharpGP2Y1010AU0F {
                 1023,
                 0,
                 REFERENCE_VOLTAGE
-            ) * WAVESHARE_DIVIDER;
+            );
             sum_voltage += voltage;
         }
-        voltage = sum_voltage / SAMPLES;
-        dust = (voltage - NODUST_VOLTAGE);
+        voltage = sum_voltage / SAMPLES; // mV
+        return voltage;
+    }
+
+    //% block
+    export function getDustValue(): number {
+        let dust = 0.0;
+        dust = CONVERSION_RATIO * (getSensorRAWValue() * WAVESHARE_DIVIDER - NODUST_VOLTAGE);
         if (dust < 0) {
             dust = 0
         }
