cc59c9d56bc439197e2a6204ecbff1d447e9acee
Raik <Raik.Andritschke@t-online.de>
2020-01-08 20:54:47 +0100

    Optional Internal reference voltage for ADC

--- test.ts
+++ test.ts
@@ -1,1 +1,22 @@
 // tests go here; this will not be compiled when this package is used as a library
+let Register = 0
+let Voltage = 0
+while (true) {
+    Register = ADCConfig.getREFSEL();
+    //basic.showNumber(Register)
+    Voltage = (pins.analogReadPin(AnalogPin.P1) * 3.25) / 1023
+    basic.showNumber(Voltage)
+    Register = ADCConfig.getREFSEL();
+    //basic.showNumber(Register)
+    ADCConfig.clearREFSEL();
+    Register = ADCConfig.getREFSEL();
+    //basic.showNumber(Register)
+    Voltage = (pins.analogReadPin(AnalogPin.P1) * 1.2 * 3) / 1023
+    basic.showNumber(Voltage)
+    Register = ADCConfig.getREFSEL();
+    //basic.showNumber(Register)
+    ADCConfig.setREFSEL();
+    Register = ADCConfig.getREFSEL();
+    //basic.showNumber(Register)
+    basic.pause(1000);
+}
